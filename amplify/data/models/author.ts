925982a758f32955744e555273a2647c4e41367c
Atul Agrawal <atul.agrawal@destiny.ws>
2025-05-23 16:39:39 +0530

    updaqted with new app files

---
+++ amplify/data/models/author.ts
@@ -0,0 +1,16 @@
+import {
+  a,
+} from '@aws-amplify/backend';
+
+export const Author = a.model({
+  id: a.string()
+    .required(),
+  name: a.string()
+    .required(),
+  email: a.string()
+    .required(),
+  
+})
+    .key('byId', ['id'])
+    .key('byEmail', ['email'])
+;
