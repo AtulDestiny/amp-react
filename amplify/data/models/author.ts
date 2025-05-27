e306a6751588faec80f4b47d1eaaf410beb7c5d7
Atul Agrawal <atul.agrawal@destiny.ws>
2025-05-27 15:43:43 +0530

    deployeing app with generated code

---
+++ amplify/data/models/author.ts
@@ -0,0 +1,20 @@
+import {
+  a,
+} from '@aws-amplify/backend';
+
+export const Author = a.model({
+  id: a.string()
+    .required(),
+  name: a.string()
+    .required(),
+  email: a.string()
+    .required(),
+  articles: a.hasMany('Article', 'authorId'),
+})
+  .key('byId', [
+    'id',
+  ])
+  .key('byEmail', [
+    'email',
+  ])
+;
