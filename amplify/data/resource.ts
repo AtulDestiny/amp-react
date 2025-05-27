de5fdf983e1d5672a99c7ba170ceb7c644c5629a
Atul Agrawal <atul.agrawal@destiny.ws>
2025-05-27 16:56:03 +0530

    only deploying models

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,56 +1,28 @@
-import {
-  type ClientSchema,
-  a,
-  defineData,
-} from '@aws-amplify/backend';
+import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
 
-import {
-  Author,
-} from './models/author';
-import {
-  GetAuthor,
-  ListAuthors,
-  GetAuthorById,
-} from './queries/author.query';
+import { Author } from "./models/author";
+import { GetAuthor, ListAuthors, GetAuthorById } from "./queries/author.query";
 import {
   CreateAuthor,
   UpdateAuthor,
   DeleteAuthor,
-} from './mutations/author.mutation';
+} from "./mutations/author.mutation";
 
-import {
-  Article,
-} from './models/article';
+import { Article } from "./models/article";
 import {
   GetArticle,
   ListArticles,
   GetArticleById,
-} from './queries/article.query';
+} from "./queries/article.query";
 import {
   CreateArticle,
   UpdateArticle,
   DeleteArticle,
-} from './mutations/article.mutation';
-
+} from "./mutations/article.mutation";
 
 const schema = a.schema({
-
   Author,
-  GetAuthor,
-  ListAuthors,
-  GetAuthorById,
-  CreateAuthor,
-  UpdateAuthor,
-  DeleteAuthor,
-
   Article,
-  GetArticle,
-  ListArticles,
-  GetArticleById,
-  CreateArticle,
-  UpdateArticle,
-  DeleteArticle,
-
 });
 
 export type Schema = ClientSchema<typeof schema>;
@@ -58,7 +30,7 @@ export type Schema = ClientSchema<typeof schema>;
 export const data = defineData({
   schema,
   authorizationModes: {
-    defaultAuthorizationMode: 'apiKey',
+    defaultAuthorizationMode: "apiKey",
     apiKeyAuthorizationMode: {
       expiresInDays: 30,
     },
