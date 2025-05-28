09754dc490dc9564f1a5f99aa01e5b5432a1e092
Atul Agrawal <atul.agrawal@destiny.ws>
2025-05-28 12:25:26 +0530

    updated article model file with indexes and composite indexes

--- amplify/data/models/article.ts
+++ amplify/data/models/article.ts
@@ -1,19 +1,23 @@
+// amplify/data/models/Article.ts
 import { a } from "@aws-amplify/backend";
 
-export const Article = a
-  .model({
+export const Article = a.model(() => ({
+  fields: {
     id: a.id().required(),
     title: a.string().required(),
     content: a.string().required(),
     createdAt: a.datetime(),
     authorId: a.string().required(),
     author: a.belongsTo("Author", "authorId"),
-  })
-  // identifier
-  .identifier(["id"])
-  // .secondaryIndexes((index) => [index("id")])
-  // // index: search by title
-  // .index("byTitle", ["title"])
-  // // composite index: query by authorId + createdAt
-  // .index("byAuthorIdCreatedAt", ["authorId", "createdAt"])
-  .authorization((allow) => [allow.publicApiKey()]);
+  },
+  identifier: {
+    name: "byId",
+    fields: ["id"], // must match exactly with fields.id
+  },
+  indexes: {
+    byAuthorCreatedAt: {
+      fields: ["authorId", "createdAt"],
+    },
+  },
+  authorization: (allow) => [allow.publicApiKey()],
+}));
