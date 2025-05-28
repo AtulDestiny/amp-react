3c753564b88aa38e67d21146bc61da627e833e27
Atul Agrawal <atul.agrawal@destiny.ws>
2025-05-28 18:14:00 +0530

    Modified article.query.ts file

--- amplify/data/queries/article.query.ts
+++ amplify/data/queries/article.query.ts
@@ -1,32 +1,29 @@
-import { a, defineQuery } from "@aws-amplify/backend";
+import { a } from "@aws-amplify/backend";
 import { Article } from "../models/article";
 
-export const GetArticle = defineQuery({
-  name: "GetArticle",
-  arguments: {
+export const GetArticle = a
+  .query()
+  .arguments({
     id: a.string().required(),
-  },
-  returns: Article,
-  resolve: async ({ args, ctx }) => {
+  })
+  .returns(Article)
+  .resolver(({ args, ctx }) => {
     return ctx.db.Article.get({ id: args.id });
-  },
-});
+  });
 
-export const ListArticles = defineQuery({
-  name: "ListArticles",
-  returns: a.array(Article),
-  resolve: async ({ ctx }) => {
+export const ListArticles = a
+  .query()
+  .returns(a.list(Article))
+  .resolver(({ ctx }) => {
     return ctx.db.Article.list();
-  },
-});
+  });
 
-export const GetArticleById = defineQuery({
-  name: "GetArticleById",
-  arguments: {
+export const GetArticleById = a
+  .query()
+  .arguments({
     id: a.string().required(),
-  },
-  returns: Article,
-  resolve: async ({ args, ctx }) => {
+  })
+  .returns(Article)
+  .resolver(({ args, ctx }) => {
     return ctx.db.Article.get({ id: args.id });
-  },
-});
+  });
