7c0dc616f07de6958521f86cb81d47b6d893753d
Atul Agrawal <atul.agrawal@destiny.ws>
2025-05-28 19:07:50 +0530

    Modified article.query.ts file

--- amplify/data/queries/article.query.ts
+++ amplify/data/queries/article.query.ts
@@ -1,15 +1,14 @@
 import { a, defineFunction } from "@aws-amplify/backend";
 import { Article } from "../models/article";
 
-// Define the handler function
-const getArticleHandler = defineFunction({
-  entry: "./handlers/get-article.handler.ts",
-});
-
 export const GetArticle = a
   .query()
   .arguments({
     id: a.string().required(),
   })
   .returns(Article)
-  .handler(a.handler.function(getArticleHandler));
+  .handler(async (event: any, context: any) => {
+    return context.db.Article.get({
+      id: event.arguments.id,
+    });
+  });
