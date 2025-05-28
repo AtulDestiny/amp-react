46244c54ef90be06efa4d288ced6461ceea99f24
Atul Agrawal <atul.agrawal@destiny.ws>
2025-05-28 17:52:24 +0530

    Modified article.query.ts file

--- amplify/data/queries/article.query.ts
+++ amplify/data/queries/article.query.ts
@@ -1,39 +1,32 @@
-import {
-  a,
-} from '@aws-amplify/backend';
-import {
-  Article,
-} from '../models/article';
+import { a } from "@aws-amplify/backend";
+import { Article } from "../models/article";
 
-export const GetArticle = a.query()
-  .arguments({
+export const GetArticle = a.query({
+  arguments: {
     id: a.string().required(),
-  })
-  .returns(Article)
-  .resolve(({
-    args, ctx,
-  }) => {
+  },
+  returns: Article,
+  resolve: ({ args, ctx }) => {
     return ctx.db.Article.get({
       id: args.id,
     });
-  });
+  },
+});
 
-export const ListArticles = a.query()
+export const ListArticles = a
+  .query()
   .returns(a.array(Article))
-  .resolve(({
-    ctx,
-  }) => {
+  .resolve(({ ctx }) => {
     return ctx.db.Article.list();
   });
 
-export const GetArticleById = a.query()
+export const GetArticleById = a
+  .query()
   .arguments({
     id: a.string().required(),
   })
   .returns(Article)
-  .resolve(async ({
-    args, ctx,
-  }) => {
+  .resolve(async ({ args, ctx }) => {
     return ctx.db.Article.get({
       id: args.id,
     });
