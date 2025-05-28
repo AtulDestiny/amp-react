04e8a32a80b842b7aec05beca2d462017a0c09c4
Atul Agrawal <atul.agrawal@destiny.ws>
2025-05-28 12:02:09 +0530

    updated article model file with indexes and composite indexes

--- amplify/data/models/article.ts
+++ amplify/data/models/article.ts
@@ -2,15 +2,16 @@ import { a } from "@aws-amplify/backend";
 
 export const Article = a
   .model({
-    id: a.string().required(),
+    id: a.id().required(),
     title: a.string().required(),
     content: a.string().required(),
     createdAt: a.datetime(),
     authorId: a.string().required(),
     author: a.belongsTo("Author", "authorId"),
   })
-  // Composite Primary Key
-  .identifier(["id"])
+  // identifier
+  // .identifier(['authorId'])
+  .secondaryIndexes((index) => [index("id")])
   // index: search by title
   .index("byTitle", ["title"])
   // composite index: query by authorId + createdAt
