ed020a9b3bd4af6368b857f90998a41bc4b6b2c4
Atul Agrawal <atul.agrawal@destiny.ws>
2025-05-28 17:23:54 +0530

    Added query files for article

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,12 +1,21 @@
 import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
 
+import { Article } from "./models/article";
+import {
+  GetArticle,
+  ListArticles,
+  GetArticleById,
+} from "./queries/article.query";
+
 import { Author } from "./models/author";
 
-import { Article } from "./models/article";
-
 const schema = a.schema({
-  Author,
   Article,
+  GetArticle,
+  ListArticles,
+  GetArticleById,
+
+  Author,
 });
 
 export type Schema = ClientSchema<typeof schema>;
