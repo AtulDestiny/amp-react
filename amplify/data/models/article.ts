8057a4273998f6319e0e021823f97063c51b1dc5
Atul Agrawal <atul.agrawal@destiny.ws>
2025-05-28 11:43:34 +0530

    updated article model file with indexes and composite indexes

--- amplify/data/models/article.ts
+++ amplify/data/models/article.ts
@@ -9,4 +9,10 @@ export const Article = a
     authorId: a.string().required(),
     author: a.belongsTo("Author", "authorId"),
   })
+  // Composite Primary Key
+  .identifier(["id"])
+  // index: search by title
+  .index("byTitle", ["title"])
+  // composite index: query by authorId + createdAt
+  .index("byAuthorIdCreatedAt", ["authorId", "createdAt"])
   .authorization((allow) => [allow.publicApiKey()]);
