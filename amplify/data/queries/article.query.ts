98e8e40a69f922f09ec11ccabc1f94354103359d
Atul Agrawal <atul.agrawal@destiny.ws>
2025-05-29 11:51:37 +0530

    added handlers for article query model

--- amplify/data/queries/article.query.ts
+++ amplify/data/queries/article.query.ts
@@ -10,8 +10,31 @@ export const GetArticle = a
   .arguments({ id: a.string().required() })
   .returns(Article)
   .authorization((allow) => [allow.publicApiKey()])
-  .handler(async (event: { arguments: { id: string } }, context: any) => {
-    return context.db.Article.get({
-      id: event.arguments.id,
-    });
-  });
+  .handler(
+    a.handler.function(
+      async (event: { arguments: { id: string } }, context: any) => {
+        return context.db.Article.get({
+          id: event.arguments.id,
+        });
+      }
+    )
+  );
+
+// export const ListArticles = a
+//   .query()
+//   .returns(a.array(Article))
+//   .resolve(({ ctx }) => {
+//     return ctx.db.Article.list();
+//   });
+
+// export const GetArticleById = a
+//   .query()
+//   .arguments({
+//     id: a.string().required(),
+//   })
+//   .returns(Article)
+//   .resolve(async ({ args, ctx }) => {
+//     return ctx.db.Article.get({
+//       id: args.id,
+//     });
+//   });
