4a896c45971cfed25b08da7c0ebecfe177141205
Atul Agrawal <atul.agrawal@destiny.ws>
2025-05-29 15:41:08 +0530

    added handlers for article query model

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,25 +1,56 @@
-import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
+import {
+  type ClientSchema,
+  a,
+  defineData,
+  defineFunction,
+} from "@aws-amplify/backend";
 
-import { Article } from "./models/article";
-import { GetArticle } from "./queries/article.query";
-
-import { Author } from "./models/author";
+const echoHandler = defineFunction({
+  entry: "./echo-handler/handler.ts",
+});
 
 const schema = a.schema({
-  Article,
-  GetArticle,
+  EchoResponse: a.customType({
+    content: a.string(),
+    executionDuration: a.float(),
+  }),
 
-  Author,
+  echo: a
+    .query()
+    .arguments({ content: a.string() })
+    .returns(a.ref("EchoResponse"))
+    .authorization((allow) => [allow.publicApiKey()])
+    .handler(a.handler.function(echoHandler)),
 });
 
 export type Schema = ClientSchema<typeof schema>;
 
 export const data = defineData({
   schema,
-  authorizationModes: {
-    defaultAuthorizationMode: "apiKey",
-    apiKeyAuthorizationMode: {
-      expiresInDays: 30,
-    },
-  },
 });
+
+// import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
+
+// import { Article } from "./models/article";
+// import { GetArticle } from "./queries/article.query";
+
+// import { Author } from "./models/author";
+
+// const schema = a.schema({
+//   Article,
+//   GetArticle,
+
+//   Author,
+// });
+
+// export type Schema = ClientSchema<typeof schema>;
+
+// export const data = defineData({
+//   schema,
+//   authorizationModes: {
+//     defaultAuthorizationMode: "apiKey",
+//     apiKeyAuthorizationMode: {
+//       expiresInDays: 30,
+//     },
+//   },
+// });
