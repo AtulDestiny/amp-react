c56c54b4ce2c9ba484385b8440ba6463500eeea7
Atul Agrawal <atul.agrawal@destiny.ws>
2025-05-30 14:26:05 +0530

    Added the authentication for model and field level

--- amplify/data/models/author.ts
+++ amplify/data/models/author.ts
@@ -5,17 +5,29 @@ export const Author = a
     authorId: a
       .string()
       .required()
-      .authorization((allow) => [allow.owner()]),
+      .authorization((allow) => [
+        allow.publicApiKey().to(["read"]),
+        allow.authenticated().to(["read"]),
+      ]),
     name: a
       .string()
       .required()
-      .authorization((allow) => [allow.owner()]),
+      .authorization((allow) => [
+        allow.publicApiKey().to(["read"]),
+        allow.authenticated().to(["read"]),
+      ]),
     email: a
       .string()
       .required()
-      .authorization((allow) => [allow.owner()]),
+      .authorization((allow) => [
+        allow.publicApiKey().to(["read"]),
+        allow.authenticated().to(["read"]),
+      ]),
     articles: a.hasMany("Article", "authorId"),
   })
   .secondaryIndexes((index: any) => [index("authorId"), index("email")])
-  .authorization((allow) => [allow.owner()]);
+  .authorization((allow) => [
+    allow.publicApiKey().to(["read"]),
+    allow.authenticated().to(["read"]),
+  ]);
 // .authorization((allow: any) => [allow.publicApiKey()]);
