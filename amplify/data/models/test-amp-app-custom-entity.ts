05fb8d20717e99db3e1d691648bbb8f738182e39
Atul Agrawal <atul.agrawal@destiny.ws>
2025-06-03 15:41:23 +0530

    Created models for virtual entity of application

--- amplify/data/models/test-amp-app-custom-entity.ts
+++ amplify/data/models/test-amp-app-custom-entity.ts
@@ -1,23 +1,21 @@
-import {
-  a,
-} from '@aws-amplify/backend';
+import { a } from "@aws-amplify/backend";
 
-export const TestAmpAppCustomEntity = a.model({
-  id: a
-    .STRING()
-                .authorization((allow: any) => [
-      allow.publicApiKey().to(['read']),
-      allow.authenticated().to(['read']),
-    ]),
-  name: a
-    .STRING()
-                .authorization((allow: any) => [
-      allow.publicApiKey().to(['read']),
-      allow.authenticated().to(['read']),
-    ]),
-  
-})
-.authorization((allow: any) => [
-  allow.publicApiKey().to(['read']),
-  allow.authenticated().to(['read']),
-]);
+export const TestAmpAppCustomEntity = a
+  .model({
+    id: a
+      .string()
+      .authorization((allow: any) => [
+        allow.publicApiKey().to(["read"]),
+        allow.authenticated().to(["read"]),
+      ]),
+    name: a
+      .string()
+      .authorization((allow: any) => [
+        allow.publicApiKey().to(["read"]),
+        allow.authenticated().to(["read"]),
+      ]),
+  })
+  .authorization((allow: any) => [
+    allow.publicApiKey().to(["read"]),
+    allow.authenticated().to(["read"]),
+  ]);
