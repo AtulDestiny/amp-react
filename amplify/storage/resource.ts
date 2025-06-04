017184d56f44c1b90434d0145d53dbe4457d9ac6
karans005 <karans@koderxpert.com>
2025-06-04 14:26:29 +0530

    Added s3 bucket code

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -1,17 +1,5 @@
 import { defineStorage } from '@aws-amplify/backend';
-import { aws_s3 as s3 } from 'aws-cdk-lib';
 
 export const storage = defineStorage({
-  name: 'amplifyTeamDrive',
-  configure: (context) => {
-    const existingBucket = s3.Bucket.fromBucketName(
-      context.scope,
-      'ExistingBucket',
-      'dev-application-lambdas'
-    );
-
-    return {
-      bucket: existingBucket,
-    };
-  },
+  name: 'amplifyTeamDrive'
 });
