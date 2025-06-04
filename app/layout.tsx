169b16e09b52c4a8c14a8a09d83e406b4b8fbd0f
karans005 <karans@koderxpert.com>
2025-06-04 11:26:52 +0530

    Added s3 bucket missing code

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./app.css";
+import "./lib/amplify";
 
 const inter = Inter({ subsets: ["latin"] });
 
---
+++ app/lib/amplify.ts
@@ -0,0 +1,22 @@
+import { Amplify } from "aws-amplify";
+
+Amplify.configure({
+  Storage: {
+    S3: {
+      bucket: "dev-application-lambdas", // your custom bucket name
+      region: "us-east-1",
+      buckets: {
+        "dev-application-lambdas": {
+          bucketName: "dev-application-lambdas",
+          region: "us-east-1",
+          paths: {
+            "public/*": {
+              guest: ["get", "list"]
+              // You can also add authenticated or other roles here
+            }
+          }
+        }
+      }
+    }
+  }
+});
