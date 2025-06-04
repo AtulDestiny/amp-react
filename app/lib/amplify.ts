6df767e6fb79208a7c78e4bc232783ce4f278169
karans005 <karans@koderxpert.com>
2025-06-04 14:53:38 +0530

    Added s3 bucket code

--- app/lib/amplify.ts
+++ app/lib/amplify.ts
@@ -3,11 +3,11 @@ import { Amplify } from "aws-amplify";
 Amplify.configure({
   Storage: {
     S3: {
-      bucket: "dev-application-lambdas", // your custom bucket name
+      bucket: "brand-workload-content-dx0n-eocw-s3-dev", // your custom bucket name
       region: "us-east-1",
       buckets: {
-        "dev-application-lambdas": {
-          bucketName: "dev-application-lambdas",
+        "brand-workload-content-dx0n-eocw-s3-dev": {
+          bucketName: "brand-workload-content-dx0n-eocw-s3-dev",
           region: "us-east-1"
         }
       }
