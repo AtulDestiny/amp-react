f44b901130d2194647175da8fd48a6c28958c59d
karans005 <karans@koderxpert.com>
2025-06-05 10:16:47 +0530

    Check Get file

--- amplify/function/handler.ts
+++ amplify/function/handler.ts
@@ -1,29 +1,26 @@
 import {
   S3Client,
   GetObjectCommand,
+  GetObjectCommandInput,
 } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
-const s3 = new S3Client({ region: "us-east-1" }); // or your region
+const s3 = new S3Client({ region: "us-east-1" });
 
 export const handler = async (event: any) => {
   const bucketName = "brand-workload-content-dx0n-eocw-s3-dev";
   const key = event.arguments.key;
 
-  try {
-    const command = new GetObjectCommand({
-      Bucket: bucketName,
-      Key: key,
-    });
+  const input: GetObjectCommandInput = {
+    Bucket: bucketName,
+    Key: key,
+  };
 
-    const signedUrl = await getSignedUrl(s3, command, { expiresIn: 60 * 5 });
+  const command = new GetObjectCommand(input);
+  const signedUrl = await getSignedUrl(s3, command, { expiresIn: 300 });
 
-    return {
-      url: signedUrl,
-      key,
-    };
-  } catch (err) {
-    console.error(err);
-    throw new Error("Could not generate pre-signed URL.");
-  }
+  return {
+    url: signedUrl,
+    key,
+  };
 };
