ef52ab64a8604cab12aa1b063b9361ce13a7d39d
Arpit Patel <arpit@destiny.ws>
2025-06-05 16:33:23 +0530

    fixed path issue

--- amplify/functions/upload-s3/resource.ts
+++ amplify/functions/upload-s3/resource.ts
@@ -2,5 +2,5 @@ import { defineFunction } from "@aws-amplify/backend";
 
 export const uploadS3Function = defineFunction({
   name: "uploadS3Function",
-  entry: "./uploadS3.ts",
+  entry: "./handler.ts",
 });
