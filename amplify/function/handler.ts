cd52bda4beda733f94d707f0d8d6817b7b110f6d
karans005 <karans@koderxpert.com>
2025-06-05 09:39:17 +0530

    Check Get file

--- amplify/function/handler.ts
+++ amplify/function/handler.ts
@@ -1,3 +1,25 @@
-export const handler = async (event: any) => {
-  return "Hello from my first function!";
+
+import AWS from 'aws-sdk';
+
+const s3 = new AWS.S3();
+
+export const handler = async (event : any) => {
+  const bucketName = 'brand-workload-content-dx0n-eocw-s3-dev';
+  const key = event.arguments.key;
+
+  try {
+    const signedUrl = s3.getSignedUrl('getObject', {
+      Bucket: bucketName,
+      Key: key,
+      Expires: 60 * 5, // URL valid for 5 minutes
+    });
+
+    return {
+      url: signedUrl,
+      key: key,
+    };
+  } catch (err) {
+    console.error(err);
+    throw new Error("Could not generate pre-signed URL.");
+  }
 };
