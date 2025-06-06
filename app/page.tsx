f03350d89b4aca52ae94081ac601e1d0908b5734
karans005 <karans@koderxpert.com>
2025-06-06 09:58:51 +0530

    Added demo app for file upload

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,14 @@
 "use client";
 
+import FileUpload from './components/FileUpload';
+
 export default function App() {
 
   return (
-    <main>
-      <h1>My todos</h1>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/nextjs/start/quickstart/nextjs-app-router-client-components/">
-          Review next steps of this tutorial.
-        </a>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm">
+        <h1 className="text-4xl font-bold mb-8">File Upload Demo</h1>
+        <FileUpload />
       </div>
     </main>
   );
