82df73c9cc9337dc2f71a7fef8aef7a078bfe6f0
karans005 <karans@koderxpert.com>
2025-06-06 13:16:27 +0530

    check deploymet

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
+  basePath: '',
 }
 
 module.exports = nextConfig
