4745b0d35028f4a934bba99810ae01b092819d8e
karans005 <karans@koderxpert.com>
2025-06-09 11:10:29 +0530

    Added metadata in the file upload

--- amplify/functions/upload-s3/handler.ts
+++ amplify/functions/upload-s3/handler.ts
@@ -4,7 +4,6 @@ import {
   PutObjectCommandInput,
 } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
-import { defineFunction } from "@aws-amplify/backend";
 
 const s3 = new S3Client({ region: "us-east-1" });
 
@@ -34,13 +33,3 @@ export const handler = async (event: any) => {
   };
 };
 
-export const uploadS3Function = defineFunction({
-  name: "uploadS3Function",
-  entry: "./handler.ts",
-  environment: {
-    BRAND_ID: "eocw",
-    CLIENT_ID: "dx0n",
-    IS_PUBLIC: "false",
-    PARENT_FOLDER_ID: "oyyv"
-  }
-});
