64e5c9adcfdec667c6872bdf2bd03350581b567f
Atul Agrawal <atul.agrawal@destiny.ws>
2025-06-11 14:10:43 +0530

    Added custom subscription file for test

---
+++ amplify/data/subscriptions/custom-table.subscription.ts
@@ -0,0 +1,20 @@
+import { a, defineFunction } from "@aws-amplify/backend";
+
+export const AddCustomMethodSubsacription = a
+  .subscription()
+  // subscribes to the 'publish' mutation
+  .for(a.ref("AddCustomMethodCustomMutation"))
+  // subscription handler to set custom filters
+  .handler(
+    a.handler.custom({
+      name: "AddCustomMethodSubsacription",
+      entry: "../../functions/execute-flow/handler.ts",
+      timeoutSeconds: 900,
+      environment: {
+        STATE_MACHINE_ARN:
+          "arn:aws:states:us-east-1:992382535498:stateMachine:dev_d3nh2xvu5kckmx_todos_flow_cb67",
+      },
+    })
+  )
+  // authorization rules as to who can subscribe to the data
+  .authorization((allow) => [allow.publicApiKey()]);
