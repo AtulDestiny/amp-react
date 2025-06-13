7a3aa6ae84c2aaea50ed31fdc43b72ea70af871d
Arpit Patel <arpit@destiny.ws>
2025-06-13 10:31:08 +0530

    fixed build issue

--- amplify/data/addItem.ts
+++ amplify/data/addItem.ts
@@ -2,9 +2,18 @@ import { Context, util } from "@aws-appsync/utils";
 import * as ddb from "@aws-appsync/utils/dynamodb";
 
 export function request(ctx: any) {
-  const item = { ...ctx.arguments };
-  const key = { id: ctx.args.id ?? util.autoId() };
-  return ddb.put({ key, item });
+  const id = ctx.args.id ?? util.autoId();
+  const now = util.time.nowISO8601();
+
+  const item = {
+    id,
+    name: ctx.args.name,
+    __typename: "Author",
+    createdAt: now,
+    updatedAt: now,
+  };
+
+  return ddb.put({ key: { id }, item });
 }
 
 export function response(ctx: any) {
