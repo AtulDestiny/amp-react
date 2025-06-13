f60c380017e08c029b6acac65736fce2762da21a
Arpit Patel <arpit@destiny.ws>
2025-06-13 11:12:56 +0530

    fixed build issue

--- amplify/data/models/article.ts
+++ amplify/data/models/article.ts
@@ -3,36 +3,36 @@ import { a } from "@aws-amplify/backend";
 export const Article = a.model({
   id: a
     .id()
-    .required(),
-    // .authorization((allow: any) => [
-    //   allow.publicApiKey().to(["create", "read"]),
-    //   allow.authenticated().to(["read"]),
-    // ]),
+    .required()
+    .authorization((allow: any) => [
+      allow.publicApiKey().to(["create", "read"]),
+      allow.authenticated().to(["read"]),
+    ]),
   title: a
     .string()
-    .required(),
-    // .authorization((allow: any) => [
-    //   allow.publicApiKey().to(["create", "read"]),
-    //   allow.authenticated().to(["read"]),
-    // ]),
+    .required()
+    .authorization((allow: any) => [
+      allow.publicApiKey().to(["create", "read"]),
+      allow.authenticated().to(["read"]),
+    ]),
   content: a
     .string()
-    .required(),
-    // .authorization((allow: any) => [
-    //   allow.publicApiKey().to(["create", "read"]),
-    //   allow.authenticated().to(["read"]),
-    // ]),
+    .required()
+    .authorization((allow: any) => [
+      allow.publicApiKey().to(["create", "read"]),
+      allow.authenticated().to(["read"]),
+    ]),
   createdAt: a
-    .datetime(),
-    // .authorization((allow: any) => [
-    //   allow.publicApiKey().to(["create", "read"]),
-    //   allow.authenticated().to(["read"]),
-    // ]),
+    .datetime()
+    .authorization((allow: any) => [
+      allow.publicApiKey().to(["create", "read"]),
+      allow.authenticated().to(["read"]),
+    ]),
   authorId: a
     .string()
     .required()
-    // .authorization((allow: any) => [
-    //   allow.publicApiKey().to(["create", "read"]),
-    //   allow.authenticated().to(["read"]),
-    // ]),
+    .authorization((allow: any) => [
+      allow.publicApiKey().to(["create", "read"]),
+      allow.authenticated().to(["read"]),
+    ]),
 });
